052b3edddf78242b7bbe200c01d06ddef9dc6de4
Polly Ren <renpollyf@gmail.com>
2023-07-18 22:18:01 -0500

    updated interpreter skeleton types and value class

--- interpreter.py
+++ interpreter.py
@@ -21,51 +21,52 @@ class Stack:
 
 class Type(Enum):
     INT = 1
-    SHORT = 2
-    LONG = 3
-    DOUBLE = 4
-    CHAR = 5
-    FLOAT = 6
-    ENUM = 7
-    STRUCT = 8
-    UNION = 9
-    VOID = 10
+    DOUBLE = 2
+    CHAR = 3
+    STRUCT = 4
+    ENUM = 5
+    UNION = 6
+    VOID = 7
 
+NUMERIC_TYPE = [Type.INT, Type.DOUBLE]
+
+class Value:
+    def __init__(self, ptr: bool, ptr_depth: int, signed: bool, tag: Type, value):
+        self.ptr = ptr
+        self.ptr_depth = ptr_depth
+        self.tag = tag
+        self.value = value
 
 class Variable:
-    def __init__(self, name: str, signed: bool, constant: bool, type: Type, array: bool, ptr: bool, value = None):
+    def __init__(self, name: str, signed: bool, constant: bool, type: Type, ptr: bool, value=None):
         self.name = name
         self.signed = signed
         self.constant = constant
         self.type = type
-        self.array = array
         self.ptr = ptr
         self.assigned = False
         self.value = Variable.assign(value)
+        # push variable onto the stack somehow
 
     def assign(self, value):
+        if value is None: return
         if self.constant and self.assigned: 
             raise Exception("cannot reassign value of a constant")
         value_length = len(value)
+        # check types
         match self.type:
             case Type.INT:
                 self.value = int(value)
-            case Type.SHORT:
-                self.value = int(value)
-            case Type.LONG:
-                self.value = int(value)
             case Type.DOUBLE:
-                self.value = float(value)
-            case Type.FLOAT:
                 self.value = float(value)
             case Type.CHAR:
                 escape = '\\' in value
                 if (not escape and value_length != 3) or (escape and value_length != 4):
                     raise Exception("invalid character: {}".format(value))
                 self.value = value
-            case Type.ENUM:
-                pass
             case Type.STRUCT:
+                pass
+            case Type.ENUM:
                 pass
             case Type.UNION:
                 pass
